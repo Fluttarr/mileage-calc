9d181650638a8418f18b130f91650834363007ed
unknown <muralismail4u@gmail.com>
2018-07-09 18:08:59 +0530

    chore(home): add input placeholder fields

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,8 +4,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     return new Center(
-      child: new Text("Home Page"),
+      child: new Form(
+        child: new ListView(
+          children: <Widget>[
+            new TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(
+                  hintText: 'Enter Value', labelText: 'Litres'),
+            ),
+            new TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(
+                  hintText: 'Enter Value', labelText: 'kilometer'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
